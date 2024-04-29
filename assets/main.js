58401e31d0da80eba366d0065826c5010fe673b0
Jonathan Di Rico <diricojonathan@gmail.com>
2024-04-29 02:15:33 +0200

    Added OSINT menu

--- assets/main.js
+++ assets/main.js
@@ -40,3 +40,66 @@ document.addEventListener('DOMContentLoaded', function () {
         input.focus();
     });
 });
+
+// Add favicon icons to the URLs
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll("a[display-favicon]");
+    links.forEach(function(link) {
+        var url = new URL(link.href);
+        var faviconUrl = "https://www.google.com/s2/favicons?domain=" + url.hostname;
+        var faviconImg = document.createElement("img");
+        faviconImg.src = faviconUrl;
+        faviconImg.style.marginRight = "5px"; // Adjust spacing as needed
+        link.insertAdjacentElement("afterbegin", faviconImg);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var selectButtons = document.querySelectorAll('.select-button button');
+
+    // Add event listener to each select button
+    selectButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var selectedValue = this.getAttribute('value');
+            var selectedName = this.getAttribute('data-name');
+            
+            // Update action attribute of the form
+            document.getElementById('myForm').setAttribute('action', selectedValue);
+
+            // Update name attribute of the input field
+            document.getElementById('search-input').setAttribute('name', selectedName);
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var selectButtons = document.querySelectorAll('.select-button button');
+
+    // Add event listener to each select button
+    selectButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var selectedValue = this.getAttribute('value');
+            var selectedName = this.getAttribute('data-name');
+
+            // Update action attribute of the form
+            document.getElementById('myForm').setAttribute('action', selectedValue);
+
+            // Update name attribute of the input field
+            document.getElementById('search-input').setAttribute('name', selectedName);
+
+            // Update the URL in the browser without causing a page reload
+            history.pushState(null, null, selectedValue);
+        });
+    });
+});
+
+$(function(){
+    // Load header.html and insert it into the header element
+    $("header").load("assets/components/menu.html");
+    // $("footer").load("assets/components/footer.html");
+    // $("#home").load("home.html");
+    // $("#sphere").load("sphere.html");
+    // $("#decode-text").load("decode-text.html");
+    // $("#search-bar").load("search-bar.html");
+});
+
